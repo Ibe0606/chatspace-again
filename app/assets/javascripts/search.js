686c81de8b2f59e59b0cb690dada9ab928b68c1f
ibe0606 <happychild.6961@gmail.com>
2019-09-23 20:57:38 +0900

    インクリメンタルサーチ前半

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,36 @@
+$(function() {
+
+var users_list = $(".user-search-result");
+
+function appendProduct(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    users_list.append(html);             
+  }
+
+  $(".chat-group-form__input").on("keyup", function() {
+    var input = $(this).val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users) {
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendProduct(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーはいません");
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    })
+  });
+});
