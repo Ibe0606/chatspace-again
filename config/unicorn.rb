2fcdcb5d22aa1c42fc770ff320acee53b407e356
ibe0606 <happychild.6961@gmail.com>
2019-10-04 16:20:01 +0900

    自動デプロイ

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,56 +1,71 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+# ../が一つ増えている
+app_path = File.expand_path('../../../', __FILE__)
 
-#アプリケーションサーバの性能を決定する
 worker_processes 1
+# currentを指定
+working_directory "#{app_path}/current"
 
-#アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+# それぞれ、sharedの中を参照するよう変更
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-#Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
 
-#ポート番号を指定
 
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+# #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+# app_path = File.expand_path('../../', __FILE__)
 
-#エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+# #アプリケーションサーバの性能を決定する
+# worker_processes 1
 
-#通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+# #アプリケーションの設置されているディレクトリを指定
+# working_directory app_path
 
-#Railsアプリケーションの応答を待つ上限時間を設定
-timeout 60
+# #Unicornの起動に必要なファイルの設置場所を指定
+# pid "#{app_path}/tmp/pids/unicorn.pid"
 
-#以下は応用的な設定なので説明は割愛
+# #ポート番号を指定
 
-preload_app true
-GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+# listen "#{app_path}/tmp/sockets/unicorn.sock"
 
-check_client_connection false
+# #エラーのログを記録するファイルを指定
+# stderr_path "#{app_path}/log/unicorn.stderr.log"
 
-run_once = true
+# #通常のログを記録するファイルを指定
+# stdout_path "#{app_path}/log/unicorn.stdout.log"
 
-before_fork do |server, worker|
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.connection.disconnect!
+# #Railsアプリケーションの応答を待つ上限時間を設定
+# timeout 60
 
-  if run_once
-    run_once = false # prevent from firing again
-  end
+# #以下は応用的な設定なので説明は割愛
 
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH => e
-      logger.error e
-    end
-  end
-end
+# preload_app true
+# GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
-after_fork do |_server, _worker|
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
-end
+# check_client_connection false
+
+# run_once = true
+
+# before_fork do |server, worker|
+#   defined?(ActiveRecord::Base) &&
+#     ActiveRecord::Base.connection.disconnect!
+
+#   if run_once
+#     run_once = false # prevent from firing again
+#   end
+
+#   old_pid = "#{server.config[:pid]}.oldbin"
+#   if File.exist?(old_pid) && server.pid != old_pid
+#     begin
+#       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+#       Process.kill(sig, File.read(old_pid).to_i)
+#     rescue Errno::ENOENT, Errno::ESRCH => e
+#       logger.error e
+#     end
+#   end
+# end
+
+# after_fork do |_server, _worker|
+#   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+# end
